2b5c721deaa54ba342de0736ef78d1d708bbc2c2
naticayul <naticayul@gmail.com>
2025-08-28 21:14:48 -0300

    funcion de suma e igual

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 let concatenar=""
 
+let num1;
+let num2;
+
 function mostrarNumero (numero){
     concatenar+=numero;
-    document.getElementById("header").textContent= concatenar
-
-
+    document.getElementById("numeroAbajo").textContent= concatenar
 }
+
+function sum(numero){
+    num1=concatenar;
+    concatenar+=numero;
+    document.getElementById("numeroArriba").textContent= concatenar
+    concatenar="" 
+}
+
+function igual(){
+    document.getElementById("numeroArriba").textContent= Number(num1)+"+"+concatenar
+    concatenar=Number(concatenar)+Number(num1)
+    document.getElementById("numeroAbajo").textContent=concatenar
+}
+
+
+document.getElementById("texto").textContent="30"-Number("30")
