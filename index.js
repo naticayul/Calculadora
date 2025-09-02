0ed93a301f680c7f1bc31ea9fbe257b5eb704d5a
naticayul <naticayul@gmail.com>
2025-09-01 21:52:13 -0300

    agreg funciones

--- index.js
+++ index.js
@@ -1,41 +1,86 @@
-let concatenar=""
+let concatenar="";
 
-let num1;
-let num2;
+let num1="";
+let num2="";
+let opCode="";
 
 function mostrarNumero (numero){
     concatenar+=numero;
     document.getElementById("numeroAbajo").textContent= concatenar
 }
-
-function sum(numero){
+function operacion(modo){
     num1=concatenar;
-    concatenar+=numero;
-    document.getElementById("numeroArriba").textContent= concatenar
-    concatenar="" 
+    concatenar="";
+    opCode=modo;
+    document.getElementById("numeroArriba").textContent= num1 + modo;
 }
 
+
+function igual(){
+    let resultado;
+    if(opCode=="+"){
+        resultado= Number(num1) + Number(concatenar)
+    }
+    if(opCode=="-"){
+        resultado= num1-concatenar
+    }
+    if(opCode=="*"){
+        resultado= num1*concatenar
+    }
+    if(opCode=="/"){
+        if(concatenar==0){
+            resultado="no se puede dividir por 0"
+        }
+        else{
+        resultado= num1/concatenar
+        }
+    }
+    document.getElementById("numeroArriba").textContent =  Number(num1) + opCode +concatenar;
+    document.getElementById("numeroAbajo").textContent = resultado;
+    concatenar=resultado;
+
+}
+
+function porcentaje(){
+    num1=concatenar;
+    concatenar/100;
+    document.getElementById("numeroArriba").textContent= concatenar+"%"
+    document.getElementById("numeroAbajo").textContent=concatenar/100
+}  
 function exponencial(){
     num1=concatenar;
     concatenar**2;
     document.getElementById("numeroArriba").textContent= concatenar+"²"
     document.getElementById("numeroAbajo").textContent=concatenar**2
 }   
+function raizCuadrada(){
+    num1=concatenar;
+    Math.sqrt(concatenar);
+    document.getElementById("numeroArriba").textContent= "√"+concatenar
+    document.getElementById("numeroAbajo").textContent=Math.sqrt(concatenar)
+}   
+function unoSobreX(){
+    num1=concatenar;
+    //  1/(concatenar);
+    
+    if(concatenar==0){
+        document.getElementById("numeroArriba").textContent= "1/"+"("+concatenar+")"
+        document.getElementById("numeroAbajo").textContent="no se puede dividir por 0"
+        }
+    else{
+        document.getElementById("numeroArriba").textContent= "1/"+"("+concatenar+")"
+        document.getElementById("numeroAbajo").textContent=1/+(concatenar)    
+        }
+} 
 
-function igual(){
-    document.getElementById("numeroArriba").textContent= Number(num1)+"+"+concatenar
-    concatenar=Number(concatenar)+Number(num1)
-    document.getElementById("numeroAbajo").textContent=concatenar
-}
-
-function borrarTodo(){
+function borrarTodo(){ //CE//
     document.getElementById("numeroArriba").textContent= ""
     document.getElementById("numeroAbajo").textContent=0
+    concatenar=""
 }
-
-function borrarAbajo(){
+function borrarAbajo(){ //C//
     document.getElementById("numeroAbajo").textContent=0
     concatenar=""
 }
 
-document.getElementById("texto").textContent="30"-Number("30")
+
